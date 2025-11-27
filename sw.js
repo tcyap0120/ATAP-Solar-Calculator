c040ad7df438259560a44fa878e672f72f8c1a43
tcyap0120 <tiencheng.yap@gmail.com>
2025-11-27 18:15:18 +0800

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'solar-calc-v4';
+const CACHE_NAME = 'solar-calc-v6';
 
 // Install event
 self.addEventListener('install', (event) => {
@@ -30,13 +30,19 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request)
       .then((response) => {
+        // Check if we received a valid response
         if (!response || response.status !== 200 || response.type !== 'basic') {
           return response;
         }
+
+        // Clone the response
         const responseToCache = response.clone();
+
+        // Cache the new resource
         caches.open(CACHE_NAME).then((cache) => {
           cache.put(event.request, responseToCache);
         });
+
         return response;
       })
       .catch(() => {
